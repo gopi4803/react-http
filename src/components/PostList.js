e7466a763db25c733e44efc05a767cfe664c9563
Gopichand Bhimavarapu <gopichand.bhimavarapu@ibm.com>
2024-08-25 16:07:37 +0530

    react-http

---
+++ src/components/PostList.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class PostList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: [],
+      errormsg:[]
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        console.log(response);
+        this.setState({ posts: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({errormsg:'Error retrieving data'})
+      });
+  }
+  render() {
+    const { posts,errormsg } = this.state;
+    return (
+      <div>
+        List of posts
+        {posts.length
+          ? posts.map((post) => <div key={post.id}>{post.title}</div>)
+          : null}
+          { errormsg ? <div>{errormsg}</div>:null}
+      </div>
+    );
+  }
+}
+
+export default PostList;
